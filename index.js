91c7d256ab38c5f4f96c34634185324339820de8
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-01 17:10:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    }
+
+    else if (distanceTravelledInFeet(start,destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    }
+
+    else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    }
+
+    else {
+        return("cannot travel that far");
+    }
+}
